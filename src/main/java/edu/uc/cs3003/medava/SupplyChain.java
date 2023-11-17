774530f1f0a0da71e1967a456a1da70607b7bd6e
Nishit Grover <grovernt@mail.uc.edu>
2023-11-16 23:13:51 -0500

    name

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -2,5 +2,7 @@ package edu.uc.cs3003.medava;
 
 public class SupplyChain {
     public static void main(String args[]) {
+        System.out.println("Hello, <Grover>! This is the WOPR speaking");
     }
+    
 }
